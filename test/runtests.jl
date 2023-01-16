8f0a15b292e2f2277a449f2c1e4e128d339a9d9e
Batia Friedman-Shaw <bfriedmanshaw@perimeterinstitute.ca>
2023-01-16 14:52:35 -0500

    scalar multiplication

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,12 @@ using Test
 x = Dual(1, 2)
 y = Dual(2, 3)
 
-z = x + y
-@test z == Dual(3, 5)
+@test +x == x
+@test -x == Dual(-1, -2)
+
+@test x + y == Dual(3, 5)
+@test x - y == Dual(-1, -1)
+
+@test 4 * x == Dual(4, 8)
+@test y * x == Dual(10, 15)
+
